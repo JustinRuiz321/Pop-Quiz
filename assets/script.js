59e33684c9d5c3fc83cefbd38ed90f00d97a31b9
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-09-08 12:59:49 -0400

    Questions and answers

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,47 @@ const returnButton = document.getElementById("returnButton")
 
 var secondsLeft = 60;
 var timeleft = 0;
+
+const quizQuestions = [
+    {
+        question: "What element is a container for all the head elements, and may include the document title, scripts, styles, meta information, and more?",
+        options: ["<br></br>", "<title></title>", "<body></body>", "<head></head>"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is the CSS property that offers extra information about something when you hover over an element?",
+        options: ["Hint", "Info Block", "Tutorial", "Tooltip"],
+        correctAnswer: 3
+    },
+    {
+        question: "CSS stands for ____ Style Sheets.",
+        options: ["Cascading", "Concept", "Concave", "Curious"],
+        correctAnswer: 0
+    },
+    {
+        question: "In CSS and HTML colors are displayed by combining these three shades of light.",
+        options: ["Muave, Salmon, Black", "Red, Green, Blue", "Yellow, Blue, Green", "Violet, Red, Orange"],
+        correctAnswer: 1
+    },
+    {
+        question: "What is the name of the statement that is used to exit or end a loop?",
+        options: ["Break Statement", "Falter Statement", "Conditional Statement", "Close Statment"],
+        correctAnswer: 0
+    },
+    {
+        question: "In JavaScript, what is used in conjunction with HTML to “react” to certain elements?",
+        options: ["RegExp" , "Boolean" , "Condition" , "Events"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+        options: ["Boolean" , "RegExp" , "Events" , "Condition"],
+        correctAnswer: 0
+    },
+    {
+        question: "What are the identifiers called that cannot be used as variables or function names?",
+        options: ["Reserved Words" , "Favorites" , "Constants" , "Concrete Terms"],
+        correctAnswer: 0
+    }
+
+];
